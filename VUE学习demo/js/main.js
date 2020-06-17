e7ceda428cdc979c9b407df7d8fe666dc3e191f9
rovesoul <397703547@qq.com>
2020-06-17 23:48:59 +0800

    VUE 4d terminal

---
+++ VUE学习demo/js/main.js
@@ -0,0 +1,38 @@
+/*
+  请求地址:http://wthrcdn.etouch.cn/weather_mini
+  请求方法:get
+  请求参数:city(城市名)
+  响应内容:天气信息
+
+  1. 点击回车
+  2. 查询数据
+  3. 渲染数据
+  */
+ var app = new Vue({
+     el:"#app",
+     data:{
+         city:'北京',
+         weatherList:[]
+     },
+     methods: {
+         searchWeather:function(){
+             console.log('天气查询');
+             console.log(this.city);
+            // 调用接口
+            // 保存this
+            var that = this;
+            axios.get('http://wthrcdn.etouch.cn/weather_mini?city='+this.city)
+            .then(function(response){
+                // console.log(response);
+                console.log(response.data.data.forecast);
+                // 保存this 的天气列表 注意是that
+                that.weatherList = response.data.data.forecast
+            })
+            .catch(function(err){})
+         },
+         changeCity:function (city) {
+             this.city = city;
+             this.searchWeather();
+         }
+     },
+ })
